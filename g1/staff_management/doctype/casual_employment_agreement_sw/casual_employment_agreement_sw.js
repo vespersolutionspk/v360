f031fdc76af6e34e06dec8dc8e4cf6e1167cfe07
Vesper <vespersolutionspk@gmail.com>
2021-02-23 03:00:02 +0000

    adding files

---
+++ g1/staff_management/doctype/casual_employment_agreement_sw/casual_employment_agreement_sw.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Vesper Solutions  and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Casual Employment Agreement SW', {
+	// refresh: function(frm) {
+
+	// }
+});
